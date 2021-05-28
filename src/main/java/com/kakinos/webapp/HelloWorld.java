1edbece23cfb7097671e27f22b4e97f2446c1139
gohittanya <gohittashu95@gmail.com>
2021-05-28 12:51:07 +0000

    commection with db

--- src/main/java/com/kakinos/webapp/HelloWorld.java
+++ src/main/java/com/kakinos/webapp/HelloWorld.java
@@ -1,13 +1,25 @@
 package com.kakinos.webapp;
 
+import com.kakinos.webapp.model.Patient;
+import com.kakinos.webapp.repository.PatientRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @EnableAutoConfiguration
 public class HelloWorld {
+
+    @Autowired
+    PatientRepository patientRepository;
+
     @RequestMapping("/")
     public ModelAndView home() {
         ModelAndView modelAndView = new ModelAndView();
@@ -31,6 +43,18 @@ public class HelloWorld {
 
         return modelAndView;
     }
+
+    @PostMapping("/patient")
+    public ResponseEntity<Patient> createTutorial(@RequestBody Patient patient) {
+    try {
+      System.out.println("hello");
+    //  Patient _patient = PatientRepository.save(new Patient(patient.getFirstName(), patient.getAge()));
+      Patient _patient = patientRepository.save(new Patient("Sunny", "IIIT", 25, "male", "Allahabad", 123456));
+        return new ResponseEntity<>(_patient, HttpStatus.CREATED);
+      } catch (Exception e) {
+        return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+      }
+  }
 
     @RequestMapping("/search_patient_form")
     public ModelAndView search_patient_form() {
---
+++ src/main/java/com/kakinos/webapp/model/Patient.java
@@ -0,0 +1,89 @@
+package com.kakinos.webapp.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "patient")
+public class Patient {
+  @Id
+  private String id;
+
+  private String first_name;
+  private String last_name;
+  private int age;
+  private String gender;
+  private String city;
+  private int pincode;
+
+  public Patient() {
+
+  }
+
+  public Patient(String first_name, String last_name, int age, String gender, String city, int pincode) {
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.age = age;
+    this.gender = gender;
+    this.city = city;
+    this.pincode = pincode;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public String getFirstName() {
+    return first_name;
+  }
+
+  public void setFirstName(String first_name) {
+    this.first_name = first_name;
+  }
+
+  public String getLastName() {
+    return last_name;
+  }
+
+  public void setLastName(String last_name) {
+    this.last_name = last_name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public String getGender() {
+    return gender;
+  }
+
+  public void setGender(String gender) {
+    this.gender = gender;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
+  }
+
+  public int getPinCode() {
+    return pincode;
+  }
+
+  public void setPinCode(int pincode) {
+    this.pincode = pincode;
+  }
+
+  
+
+  @Override
+  public String toString() {
+    return "Patient [id=" + id + ", first_name=" + first_name + ", last_name=" + last_name + ", age="  + age + ", gender=" + gender + ", city="  + city + ", pincode=" + pincode + "]";
+  }
+}
